1476587a604f4b38ebbbf86703d6e8ca72d4977b
nereamugu <nereamuguruza22@gmail.com>
2022-05-28 18:42:08 +0100

    rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import CharacterList from './views/CharacterList';
+import Contact from './views/Contacto';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <div className="App">
-      <CharacterList />
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route path="/" element={<CharacterList />} />
+				<Route path="/contact" element={<Contact />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
+
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,26 @@
-import React from "react"
-import SearchBar from "../SearchBar";
-import "./styles.css"
+import React from 'react';
 
+import { Link } from 'react-router-dom';
+
+import './styles.css';
 
 const Header = () => {
-    return (<div className="header__background">
-        <div className="header__logo">Rick and Morty</div>
-        <div className="header__navigation">
-            <p className="navigation__item"> Home</p>
-            <p className="navigation__item"> Contacto</p>
-        </div>
-    </div>);
+	return (
+		<div className="header__background">
+			<div className="header__logo">Rick and Morty</div>
+			<div className="header__navigation">
+				<Link to="/">
+					<p className="navigation__item"> Home</p>
+				</Link>
+				<Link to="/contact">
+					{' '}
+					<p className="navigation__item"> Contacto</p>
+				</Link>
+			</div>
+		</div>
+	);
 
-{/* <Link to="/">Home</Link>
-<Link to="/pokemons">Pokemons</Link>
-<Link to="/contacto">Contacto</Link> */}
-
-}
+	{
+	}
+};
 export default Header;
