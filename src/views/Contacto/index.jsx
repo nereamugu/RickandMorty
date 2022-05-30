3ad0c1b8d95c052fc71c6f99cb0fc2af7c8daa3c
nereamugu <nereamuguruza22@gmail.com>
2022-05-30 16:20:13 +0100

    cambios diseño

--- src/views/Contacto/index.jsx
+++ src/views/Contacto/index.jsx
@@ -26,32 +26,50 @@ const Contact = () => {
             <h2>Página de contacto</h2>
             </div>
 
-            <div className="form__container">
-            <div className="formulario__columna1">
+            <div className="contenedor">
+            <div className="contenedor__formulario">
+            <div className="contact-form">
+            
+            <div className="inputs-fields">
             <form>
                 <input 
+                
                 type="text"
                 name="Nombre"
                 placeholder="Nombre y apellidos"
                 onBlur={(e) => handleForm(e)} 
                 /><br />
 
+                <input 
+                
+                type="text"
+                name="phone"
+                placeholder="Telefono"
+                onBlur={(e) => handleForm(e)} 
+                /><br />
+
+
             <input 
                 type="email"
                 name= "email"
-                placeholder="Correo"
+                className="msg"
+                placeholder="mensajae"
                 onBlur={(e) => handleForm(e)}
                 /><br />
-                <textarea name="mensaje"  cols="30" rows="10" onBlur={(e) => handleForm(e)}></textarea><br />
-                <span onClick={() => sendForm()}>Enviar</span>
+
+                <div className="msg">
+                <textarea name="Mensjae"  cols="30" rows="30" onBlur={(e) => handleForm(e)}></textarea><br />
+                <div class="btn"><span onClick={() => sendForm()}>Enviar</span></div>
+                </div> 
                  
 
             </form>
             </div>
-            <div className="formulario__columna2">
-                <img src={contacto} alt="" />
             </div>
             </div>
+            </div>
+            
+            
             <Footer />
         </div>
         
@@ -61,3 +79,40 @@ const Contact = () => {
 };
 
 export default Contact;
+
+
+
+// return(
+//     <div>
+//         <div className="contact-header__container">
+//         <h2>Página de contacto</h2>
+//         </div>
+
+//         <div className="form__container">
+//         <div className="formulario__columna1">
+//         <form>
+//             <input 
+//             type="text"
+//             name="Nombre"
+//             placeholder="Nombre y apellidos"
+//             onBlur={(e) => handleForm(e)} 
+//             /><br />
+
+//         <input 
+//             type="email"
+//             name= "email"
+//             placeholder="Correo"
+//             onBlur={(e) => handleForm(e)}
+//             /><br />
+//             <textarea name="mensaje"  cols="30" rows="10" onBlur={(e) => handleForm(e)}></textarea><br />
+//             <span onClick={() => sendForm()}>Enviar</span>
+             
+
+//         </form>
+//         </div>
+//         <div className="formulario__columna2">
+//             <img src={contacto} alt="" />
+//         </div>
+//         </div>
+//         <Footer />
+//     </div>
